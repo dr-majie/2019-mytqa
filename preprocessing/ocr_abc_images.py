7fd3dd95ca224fcb14099914d9ede029ab9dfd41
[dr-majie] <[dr.majie@foxmail.com]>
2020-03-09 17:15:24 +0800

    update some functions of the ocr and read python file

--- preprocessing/ocr_abc_images.py
+++ preprocessing/ocr_abc_images.py
@@ -1,11 +1,15 @@
-#-----------------------------------------------
-#!/usr/bin/env python
+# -----------------------------------------------
+# !/usr/bin/env python
 # _*_coding:utf-8 _*_
-#@Time:2020/3/2 10:13
-#@Author:Ma Jie
-#@FileName: preprocessing.py
-#-----------------------------------------------
+# @Time:2020/3/2 10:13
+# @Author:Ma Jie
+# @FileName: preprocessing.py
+# -----------------------------------------------
 import requests
+import os
+import json
+from PIL import Image
+import os
 
 
 def ocr_space_file(filename, overlay=True, api_key='64f27b7a7588957', language='eng'):
@@ -35,135 +39,93 @@ def ocr_space_file(filename, overlay=True, api_key='64f27b7a7588957', language='
     return r.content.decode()
 
 
-def ocr_space_url(url, overlay=False, api_key='64f27b7a7588957', language='eng'):
-    """ OCR.space API request with remote file.
-        Python3.5 - not tested on 2.7
-    :param url: Image url.
-    :param overlay: Is OCR.space overlay required in your response.
-                    Defaults to False.
-    :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
-    :param language: Language code to be used in OCR.
-                    List of available language codes can be found on https://ocr.space/OCRAPI
-                    Defaults to 'en'.
-    :return: Result in JSON format.
-    """
-
-    payload = {'url': url,
-               'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload
-                      )
-    return r.content.decode()
+def get_img_list(file_path):
+    img_list = [img for img in os.listdir(file_path) if not os.path.isdir(img) and img.endswith('png')]
+    img_list.sort()
+    return img_list
 
 
-
-# Use examples:
-"""
-test_file_raw = ocr_space_file(filename='/data/kf/majie/dataset/tqa_train_val_test/train/abc_question_images/aquifers_16530.png', language='eng')
-test_file=json.loads(test_file_raw)
-
-
-with open("/data/kf/majie/dataset/tqa_train_val_test/train/abc_question_images/record.json", "r") as f:
-    test_file = json.load(f)
-    
-print(type(test_file))
-
-print(json_text)
-print(type(json_text))
-
-
-print(test_file)
-print(type(test_file))
-for item in test_file.items():
-    print(item)
-print(type(test_file['ParsedResults'][0]))
-print(type(test_file['ParsedResults'][0]['TextOverlay']))
-print(type(test_file['ParsedResults'][0]['TextOverlay']['Lines']))
-"""
-
-
-"""
-texts=test_file['ParsedResults'][0]['TextOverlay']['Lines']
-text_list=[]
-for text in texts:
-    #print((text['Words'][0]))
-    dict_text=text['Words'][0]
-    dict_text['Center']=(dict_text['Left']+dict_text['Width']/2 , dict_text['Top']-dict_text['Height']/2)
-    text_list.append(dict_text)
-#print(text_list)
-
-image_info={}
-image_info['Name']="aquifers_16530.png"
-image_info['Words']=text_list
-print(image_info)
-
-with open('/data/kf/majie/dataset/tqa_train_val_test/train/abc_question_images/atest.json', 'w') as json_file:
-        json.dump(image_info,json_file)
-        json_file.write('\n')
-        print("ok1")
-        json.dump(image_info, json_file)
-        json_file.write('\n')
-        print("ok2")
-        json.dump(image_info, json_file)
-        print("ok3")
-
-"""
-
-# test_url = ocr_space_url(url='http://i.imgur.com/31d5L5y.jpg')
-# print(test_url)
-keylist = [chr(i) for i in range(65, 91)]
-keylist += [chr(i) for i in range(97, 123)]
-#print(keylist)
-def Not_Alpha(s):
-    if s in keylist:
+def is_alpha(character):
+    if ord(character[0].lower()) in range(97, 123):
+        return 1
+    else:
         return 0
-    return 1
-#print(Not_alpha('djawl'))
 
 
+def is_existing(character, img_info):
+    for char_dict in img_info:
+        if char_dict['WordText'] is character:
+            print(character, char_dict['WordText'])
+            print('{} is in img info list'.format(character))
+            return 0
+    else:
+        print('{} is not in img info list'.format(character))
+        return 1
 
 
+def write_info(info, img, img_path):
+    json_dict = {}
+    img_info = []
+    img_info_json_path = os.path.join(img_path, 'abc_question_images.json')
+
+    for line_text in info:
+        char_dict = {}
+        detailed_char_info = {}
+
+        if len(line_text['LineText']) == 1 and is_alpha(line_text['LineText']):
+            if is_existing(line_text['LineText'], img_info):
+                detailed_char_info['Center'] = (line_text['Words'][0]['Left'] + line_text['Words'][0]['Width'] / 2,
+                                                line_text['Words'][0]['Top'] + line_text['Words'][0]['Height'] / 2)
+                detailed_char_info['Left'] = line_text['Words'][0]['Left']
+                detailed_char_info['Top'] = line_text['Words'][0]['Top']
+                detailed_char_info['Height'] = line_text['Words'][0]['Height']
+                detailed_char_info['Width'] = line_text['Words'][0]['Width']
+
+                char_dict['WordText'] = line_text['Words'][0]['WordText']
+                char_dict['Coordinate'] = detailed_char_info
+
+                img_info.append(char_dict)
+
+        elif len(line_text['LineText']) == 1 and (not is_alpha(line_text['LineText'])):
+            pass
+
+        elif len(line_text['LineText']) == 2 and line_text['LineText'].startswith('L') and is_alpha(
+                line_text['Words'][0]['WordText'][1]):
+
+            if is_existing(line_text['Words'][0]['WordText'][1], img_info):
+                detailed_char_info['Center'] = (line_text['Words'][0]['Left'] + line_text['Words'][0]['Width'] / 2,
+                                                line_text['Words'][0]['Top'] + line_text['Words'][0]['Height'] / 2)
+                detailed_char_info['Left'] = line_text['Words'][0]['Left']
+                detailed_char_info['Top'] = line_text['Words'][0]['Top']
+                detailed_char_info['Height'] = line_text['Words'][0]['Height']
+                detailed_char_info['Width'] = line_text['Words'][0]['Width']
+
+                char_dict['WordText'] = line_text['Words'][0]['WordText'][1]
+                char_dict['Coordinate'] = detailed_char_info
+
+                img_info.append(char_dict)
+        else:
+            pass
+
+    json_dict[img] = img_info
+
+    with open(img_info_json_path, 'a') as f:
+        json.dump(json_dict, f)
+        f.write('\n')
 
 
-import os
-import json
-url_lists=['train','val','test']
-for url_list in url_lists:
-    url = '/data/kf/majie/dataset/tqa_train_val_test/'+url_list+'/abc_question_images'
-    #print(url)
-    file_list = os.listdir(url)
-    write_url = '/data/kf/majie/dataset/tqa_train_val_test/' + url_list + '/abc_question_images/abc_images_info.json'
-    with open(write_url, 'w') as json_file:
-        json_file.write('\n')
-    for file in file_list[0:3]:
-        dirs = url +'/'+ file
-        #print(dirs)
-        ocr_file = ocr_space_file(filename=dirs,language='eng')
-        json_file = json.loads(ocr_file)
-        texts = json_file['ParsedResults'][0]['TextOverlay']['Lines']
-        text_list = []
-        for text in texts:
-            # print((text['Words'][0]))
-            dict_text = text['Words'][0]
-            if Not_Alpha(dict_text['WordText']):
-                print("************************")
-                print(dict_text)
-                print(dirs)
-            dict_text['Center'] = (dict_text['Left'] + dict_text['Width'] / 2, dict_text['Top'] - dict_text['Height'] / 2)
-            text_list.append(dict_text)
+if __name__ == '__main__':
+    slice_path_list = ['train', 'val', 'test']
 
-        image_info = {}
-        image_info['Name'] = file
-        image_info['Words'] = text_list
-        #print(image_info)
+    for slice_path in slice_path_list:
+        img_path = '/data/kf/majie/codehub/2019-mytqa/data/' + slice_path + '/abc_question_images/'
+        img_list = get_img_list(img_path)
 
-        write_url='/data/kf/majie/dataset/tqa_train_val_test/'+url_list+'/abc_question_images/abc_images_info.json'
-        with open(write_url,'a') as json_file:
-                json.dump(image_info, json_file)
-                json_file.write('\n')
-    print(url_list+" write ok!")
-
+        for img in img_list:
+            img_size = os.path.getsize(os.path.join(img_path, img))
+            info = ocr_space_file(os.path.join(img_path, img))
+            print(info)
+            info = json.loads(info)
+            print(img)
+            info = info['ParsedResults'][0]['TextOverlay']['Lines']
+            write_info(info, img, img_path)
--- preprocessing/read_json.py
+++ preprocessing/read_json.py
@@ -4,6 +4,7 @@ import shutil
 import nltk
 from nltk.tokenize import word_tokenize
 from preprocessing.get_closest_sen import get_closest_sentences
+from __future__ import division
 
 
 class read_json():
@@ -112,11 +113,45 @@ class read_json():
                         img_kind = str(img_path).split('/')[0]
 
                         if img_kind.startswith('abc'):
-                            pass
-                        else:
-                            img_name = img_path_tag.split('/')[-1]
+                            img_name = img_path.split('/')[-1]
+
+                            imgs_info_path = os.path.join(self.json_dir, 'abc_question_images/abc_question_images.json')
+                            imgs_info = open(imgs_info_path, 'r')
+                            imgs_info_dict = json.load(imgs_info)
+                            img_info = imgs_info_dict[img_name]
+
                             with open(q_dir + 'coordinate.txt', 'w') as f:
-                                json.dump(lessons['diagramAnnotations'][img_name], f)
+                                img_info_dict = {}
+                                img_info_dict[img_name] = img_info
+                                json.dump(img_info_dict, f)
+                        else:
+                            img_name = img_path.split('/')[-1]
+                            with open(q_dir + 'coordinate.txt', 'w') as f:
+                                img_info = lessons['diagramAnnotations'][img_name]
+
+                                img_info_list = []
+                                json_dict = {}
+                                for info in img_info:
+                                    # saving the WordText and all the word coordinate
+                                    word_dict = {}
+                                    # saving the detailed coordinate of each word
+                                    detailed_word_info = {}
+                                    detailed_word_info['Center'] = (
+                                    info['rectangle'][0][0] + (info['rectangle'][1][0] - info['rectangle'][0][0]) / 2,
+                                    info['rectangle'][0][1] + (info['rectangle'][1][1] - info['rectangle'][0][1]) / 2)
+
+                                    detailed_word_info['Left'] = info['rectangle'][0][0]
+                                    detailed_word_info['Top'] = info['rectangle'][1][1]
+                                    detailed_word_info['Height'] = info['rectangle'][1][1] - info['rectangle'][0][1]
+                                    detailed_word_info['Width'] = info['rectangle'][1][0] - info['rectangle'][0][0]
+
+                                    word_dict['WordText'] = info['rectangle']['text']
+                                    word_dict['Coordinate'] = detailed_word_info
+
+                                    img_info_list.append(word_dict)
+
+                                json_dict[img_name] = img_info_list
+                                json.dump(json_dict, f)
 
                         if not self.is_test_data:
                             correct_answer = lessons[qs_tag][q_tag][q_id]['correctAnswer']['processedText']
@@ -124,159 +159,159 @@ class read_json():
                             corr_f_handle.write(correct_answer + '\n')
                             corr_f_handle.close()
 
-    def get_questions_id(self):
+def get_questions_id(self):
+    l_id_tag = 'globalID'
+    qs_tag = 'questions'
+    ndq_tag = 'nonDiagramQuestions'
+    dq_tag = 'diagramQuestions'
+    q_tags = [ndq_tag, dq_tag]
+    q_ids_list = []
+    for f in self.json_file_list:
+        with open(os.path.join(self.json_dir, f), 'r') as f:
+            data = json.load(f)
+        for lessons in data:
+            for q_tag in q_tags:
+                for q_id, _ in lessons[qs_tag][q_tag].iteritems():
+                    q_ids_list.append(q_id)
 
-        l_id_tag = 'globalID'
-        qs_tag = 'questions'
-        ndq_tag = 'nonDiagramQuestions'
-        dq_tag = 'diagramQuestions'
-        q_tags = [ndq_tag, dq_tag]
-        q_ids_list = []
-        for f in self.json_file_list:
-            with open(os.path.join(self.json_dir, f), 'r') as f:
-                data = json.load(f)
-            for lessons in data:
-                for q_tag in q_tags:
-                    for q_id, _ in lessons[qs_tag][q_tag].iteritems():
-                        q_ids_list.append(q_id)
+    return q_ids_list
 
-        return q_ids_list
 
-    def sanity_test(self):
+def sanity_test(self):
+    print(20 * '*')
+    print('DOING SANITY TESTING')
 
-        print(20 * '*')
-        print('DOING SANITY TESTING')
+    lessons = self.get_list_of_dirs(self.op_dir)
+    que_fname = 'Question'
+    corr_ans_fname = 'correct_answer'
+    closest_sent_fname = 'closest_sent'
+    f_ext = '.txt'
+    num_of_ques = 0
+    wrong_que = 0
 
-        lessons = self.get_list_of_dirs(self.op_dir)
-        que_fname = 'Question'
-        corr_ans_fname = 'correct_answer'
-        closest_sent_fname = 'closest_sent'
-        f_ext = '.txt'
-        num_of_ques = 0
-        wrong_que = 0
+    for lesson in lessons:
+        print('Lesson : ', lesson)
+        questions_list = self.get_list_of_dirs(os.path.join(self.op_dir, lesson))
 
-        for lesson in lessons:
-            print('Lesson : ', lesson)
-            questions_list = self.get_list_of_dirs(os.path.join(self.op_dir, lesson))
+        for que in questions_list:
+            num_of_ques += 1
+            print('Question : ', que)
+            que_dir_path = os.path.join(self.op_dir, lesson, que)
+            file_list = self.get_list_of_files(que_dir_path, file_extension='.txt')
 
-            for que in questions_list:
-                num_of_ques += 1
-                print('Question : ', que)
-                que_dir_path = os.path.join(self.op_dir, lesson, que)
-                file_list = self.get_list_of_files(que_dir_path, file_extension='.txt')
+            if que_fname + f_ext not in file_list:
+                print("Question file doesn't exist")
 
-                if que_fname + f_ext not in file_list:
-                    print("Question file doesn't exist")
+            if corr_ans_fname + f_ext not in file_list:
+                print("Correct answer file doesn't exist")
 
-                if corr_ans_fname + f_ext not in file_list:
-                    print("Correct answer file doesn't exist")
+            if que.startswith('NDQ') and closest_sent_fname + f_ext not in file_list:
+                print("Closest sentence file doesn't exist")
 
-                if que.startswith('NDQ') and closest_sent_fname + f_ext not in file_list:
-                    print("Closest sentence file doesn't exist")
+            if not self.is_test_data:
+                with open(os.path.join(que_dir_path, corr_ans_fname + f_ext), 'r') as f:
+                    correct_answer = f.readlines()
 
-                if not self.is_test_data:
-                    with open(os.path.join(que_dir_path, corr_ans_fname + f_ext), 'r') as f:
-                        correct_answer = f.readlines()
-
-                    correct_answer = correct_answer[0].strip().lower()
-                    option = 'a'
-
-                    while ord(option) <= ord(correct_answer):
-                        if option + f_ext not in file_list:
-                            wrong_que += 1
-                            print('Correct answer is : ', correct_answer)
-                            print("Error : Option file doesn't exist", option + f_ext)
-                            shutil.rmtree(que_dir_path)
-                            break
-                        option = chr(ord(option) + 1)
-                else:
-                    print('cant check correctness of options as this is test data')
-
-            print(20 * '**')
-
-        print('Total Questions : ', num_of_ques)
-        print('Questions with error : ', wrong_que)
-
-    def get_statistics(self):
-
-        print(20 * '*')
-        print('GETIING STATISTICS FOR THE DATA')
-
-        lessons = self.get_list_of_dirs(self.op_dir)
-        que_fname = 'Question'
-        corr_ans_fname = 'correct_answer'
-        closest_sent_fname = 'closest_sent'
-        f_ext = '.txt'
-        num_que_token_list = []
-        num_opt_token_list = []
-        num_sent_token_list = []
-
-        for lesson in lessons:
-            questions_list = self.get_list_of_dirs(os.path.join(self.op_dir, lesson))
-            for que in questions_list:
-                que_dir_path = os.path.join(self.op_dir, lesson, que)
-                file_list = self.get_list_of_files(que_dir_path, file_extension='.txt')
-
-                with open(os.path.join(que_dir_path, que_fname + f_ext), 'r') as f:
-                    ques = f.readlines()
-
-                num_of_tokens_in_que = 0
-                for sent in ques:
-                    words = word_tokenize(sent)
-                    num_of_tokens_in_que += len(words)
-
-                num_que_token_list.append(num_of_tokens_in_que)
-
-                # process DQ and NDQ
-                with open(os.path.join(que_dir_path, closest_sent_fname + f_ext), 'r') as f:
-                    sents = f.readlines()
-                num_of_tokens_in_sents = 0
-                for sent in sents:
-                    words = word_tokenize(sent)
-                    num_of_tokens_in_sents += len(words)
-                num_sent_token_list.append(num_of_tokens_in_sents)
-                if num_of_tokens_in_sents == 0:
-                    print('Lesson : ', lesson)
-                    print('Question : ', que)
-
+                correct_answer = correct_answer[0].strip().lower()
                 option = 'a'
-                while os.path.exists(os.path.join(que_dir_path, option + f_ext)):
-                    with open(os.path.join(que_dir_path, option + f_ext), 'r') as f:
-                        opt = f.readlines()
 
-                    num_of_tokens_in_opt = 0
-                    for sent in opt:
-                        words = word_tokenize(sent)
-                        num_of_tokens_in_opt += len(words)
-
-                    num_opt_token_list.append(num_of_tokens_in_opt)
+                while ord(option) <= ord(correct_answer):
+                    if option + f_ext not in file_list:
+                        wrong_que += 1
+                        print('Correct answer is : ', correct_answer)
+                        print("Error : Option file doesn't exist", option + f_ext)
+                        shutil.rmtree(que_dir_path)
+                        break
                     option = chr(ord(option) + 1)
+            else:
+                print('cant check correctness of options as this is test data')
 
-        que_lenth_dict = nltk.FreqDist(x for x in num_que_token_list)
-        print('Question length info')
+        print(20 * '**')
 
-        for k, v in que_lenth_dict.most_common(50):
-            print(str(k), str(v))
+    print('Total Questions : ', num_of_ques)
+    print('Questions with error : ', wrong_que)
 
-        print('Max question length : ', max(num_que_token_list))
-        opt_lenth_dict = nltk.FreqDist(x for x in num_opt_token_list)
 
-        print('Option length info')
-        for k, v in opt_lenth_dict.most_common(50):
-            print(str(k), str(v))
+def get_statistics(self):
+    print(20 * '*')
+    print('GETIING STATISTICS FOR THE DATA')
 
-        print('Max Option length : ', max(num_opt_token_list))
-        sent_lenth_dict = nltk.FreqDist(x for x in num_sent_token_list)
-        print('Closest sentence info')
+    lessons = self.get_list_of_dirs(self.op_dir)
+    que_fname = 'Question'
+    corr_ans_fname = 'correct_answer'
+    closest_sent_fname = 'closest_sent'
+    f_ext = '.txt'
+    num_que_token_list = []
+    num_opt_token_list = []
+    num_sent_token_list = []
 
-        for k, v in sent_lenth_dict.most_common(1000):
-            print(str(k), str(v))
-        print('Max Closest sentence length : ', max(num_sent_token_list))
+    for lesson in lessons:
+        questions_list = self.get_list_of_dirs(os.path.join(self.op_dir, lesson))
+        for que in questions_list:
+            que_dir_path = os.path.join(self.op_dir, lesson, que)
+            file_list = self.get_list_of_files(que_dir_path, file_extension='.txt')
 
-    def read_json_do_sanity_create_closest_sent_(self, word2vec_path):
-        self.read_content()
-        processed_data_path = os.path.dirname(self.op_dir)
-        read_training_json = get_closest_sentences(processed_data_path)
-        read_training_json.generate_closest_sentence(word2vec_path)
-        self.get_statistics()
-        self.sanity_test()
+            with open(os.path.join(que_dir_path, que_fname + f_ext), 'r') as f:
+                ques = f.readlines()
+
+            num_of_tokens_in_que = 0
+            for sent in ques:
+                words = word_tokenize(sent)
+                num_of_tokens_in_que += len(words)
+
+            num_que_token_list.append(num_of_tokens_in_que)
+
+            # process DQ and NDQ
+            with open(os.path.join(que_dir_path, closest_sent_fname + f_ext), 'r') as f:
+                sents = f.readlines()
+            num_of_tokens_in_sents = 0
+            for sent in sents:
+                words = word_tokenize(sent)
+                num_of_tokens_in_sents += len(words)
+            num_sent_token_list.append(num_of_tokens_in_sents)
+            if num_of_tokens_in_sents == 0:
+                print('Lesson : ', lesson)
+                print('Question : ', que)
+
+            option = 'a'
+            while os.path.exists(os.path.join(que_dir_path, option + f_ext)):
+                with open(os.path.join(que_dir_path, option + f_ext), 'r') as f:
+                    opt = f.readlines()
+
+                num_of_tokens_in_opt = 0
+                for sent in opt:
+                    words = word_tokenize(sent)
+                    num_of_tokens_in_opt += len(words)
+
+                num_opt_token_list.append(num_of_tokens_in_opt)
+                option = chr(ord(option) + 1)
+
+    que_lenth_dict = nltk.FreqDist(x for x in num_que_token_list)
+    print('Question length info')
+
+    for k, v in que_lenth_dict.most_common(50):
+        print(str(k), str(v))
+
+    print('Max question length : ', max(num_que_token_list))
+    opt_lenth_dict = nltk.FreqDist(x for x in num_opt_token_list)
+
+    print('Option length info')
+    for k, v in opt_lenth_dict.most_common(50):
+        print(str(k), str(v))
+
+    print('Max Option length : ', max(num_opt_token_list))
+    sent_lenth_dict = nltk.FreqDist(x for x in num_sent_token_list)
+    print('Closest sentence info')
+
+    for k, v in sent_lenth_dict.most_common(1000):
+        print(str(k), str(v))
+    print('Max Closest sentence length : ', max(num_sent_token_list))
+
+
+def read_json_do_sanity_create_closest_sent_(self, word2vec_path):
+    self.read_content()
+    processed_data_path = os.path.dirname(self.op_dir)
+    read_training_json = get_closest_sentences(processed_data_path)
+    read_training_json.generate_closest_sentence(word2vec_path)
+    self.get_statistics()
+    self.sanity_test()
