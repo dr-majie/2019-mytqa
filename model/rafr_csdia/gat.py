c51b55b352efaabd9efa4febe0f4232c5cfe5b18
dr-majie <dr.majie@foxmail.com>
2021-03-09 16:17:18 +0800

    update rafr_tqa.layer to rafr_csdia.

--- model/rafr_csdia/gat.py
+++ model/rafr_csdia/gat.py
@@ -9,7 +9,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model.rafr_tqa.layer import GraphAttentionLayer
+from model.rafr_csdia.layer import GraphAttentionLayer
 
 
 class GAT(nn.Module):
