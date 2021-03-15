be15e29955f2acaa4c05e21ef577861641acd4be
dr-majie <dr.majie@foxmail.com>
2021-03-15 14:51:11 +0800

    add a space into 'using val/test split.'

--- model/rafr_tqa/test2.py
+++ model/rafr_tqa/test2.py
@@ -10,6 +10,7 @@ from model.rafr_tqa.gat import GAT
 import torch
 import numpy as np
 
+"""
 np.set_printoptions(threshold=np.inf)
 adjacency_path = '/data/kf/majie/wangyaxian/2019-tqa/data/train/processed_data/graph_files/L_0002/NDQ_000046/a.pkl'
 emb_path = '/data/kf/majie/wangyaxian/2019-tqa/data/train/processed_data/graph_files/L_0002/NDQ_000046/embedding_a.pkl'
@@ -30,3 +31,7 @@ for i, vec in enumerate(emb.values()):
 model = GAT(emb_np.shape[1], 300, 300, 0., 0.2, 8)
 out = model(torch.FloatTensor(emb_np), matrix)
 out = out.detach().numpy()
+"""
+test = 'A.5,10,20,30 B.5,10,15,20 C.10,20,30 D.5'
+test = test.strip().split()
+print(test)
